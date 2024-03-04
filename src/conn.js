65e8a7ffdc818ede2dab64e1e7184c77fd0d2838
Bryan España <bryanespana21550@gmail.com>
2024-03-04 07:44:49 -0600

    update

---
+++ src/conn.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'bryan',
+    database: 'blogVR_db',
+    password: 'bryan123',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+export default pool
