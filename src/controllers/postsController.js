5a0f2f2057ecf870abac7996d6a08491bfab62f8
Bryan España <bryanespana21550@gmail.com>
2024-03-10 12:57:37 -0600

    Actualizar endpoints

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,42 +1,19 @@
 import pool from '../dbConfig/conn.js';
-// Si desea probar con data real con postman mandar en el response json(DataPrueba)
-const DataPrueba = [
-  {
-    id: 1,
-    name_device: 'Post 1',
-    relase_date:'00-00-00',
-    field_view: '360',
-    weight:'50kg',
-    relase_price:'$1000',
-    resolution_eye: 'Contenido del post 1',
-  },
-  {
-    id: 1,
-    name_device: 'Post 1',
-    relase_date:'00-00-00',
-    field_view: '360',
-    weight:'50kg',
-    relase_price:'$1000',
-    resolution_eye: 'Contenido del post 1',
-  },
-  {
-    id: 1,
-    name_device: 'Post 1',
-    relase_date:'00-00-00',
-    field_view: '360',
-    weight:'50kg',
-    relase_price:'$1000',
-    resolution_eye: 'Contenido del post 1',
-  },
-];
 // listar todos los post
 export const getAllPosts = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM blogs');
-    res.status(200).json(rows);
+    res.status(200).json({
+      code: 200, 
+      message: 'Post actualizado',
+      data: rows
+    });
   } catch (err) {
     console.error('Error al crear el post:', err);
-    res.status(500).json({ message: 'Error al obtener los posts', error: err });
+    res.status(500).json({
+       code: 500,
+       message: 'Error al obtener los posts', 
+       error: err });
   }
 };
 
@@ -44,46 +21,86 @@ export const getAllPosts = async (req, res) => {
 export const getPostById = async (req, res) => {
   try {
     const [rows] = await pool.query('SELECT * FROM blogs WHERE id = ?', [req.params.id]);
-    res.status(200).json(rows);
+    if (rows.length > 0) {
+      res.status(200).json({ 
+        code: 200,
+        message: 'Post recuperado exitosamente',
+        data: rows[0]
+      });
+    } else {
+      res.status(404).json({ 
+        code: 404,
+        message: 'Post no encontrado',
+        description: `No se encontró el post con el id ${req.params.id}`
+      });
+    }
   } catch (err) {
-    res.status(500).json({ message: 'Error al obtener el post', error: err });
+    res.status(500).json({ 
+      code: 500,
+      message: 'Error al obtener el post',
+      description: err.message 
+    });
   }
 };
+
 
 // Crear un nuevo post
 export const createPost = async (req, res) => {
   try {
     const { name_device, resolution_eye, relase_date, field_view, weight, relase_price } = req.body;
     await pool.query('INSERT INTO blogs (name_device, resolution_eye, relase_date, field_view, weight, relase_price) VALUES (?, ?, ?, ?, ?, ?)', [name_device, resolution_eye, relase_date, field_view, weight, relase_price]);
-    res.status(201).json({ message: 'Post creado' });
+    res.status(201).json({ 
+      code: 201,
+      message: 'Post creado exitosamente'
+    });
   } catch (err) {
     console.error('Error al crear el post:', err);
-
-    res.status(500).json({ message: 'Error al crear el post', error: err.message });
+    res.status(500).json({ 
+      code: 500,
+      message: 'Error al crear el post',
+      description: err.message 
+    });
   }
 };
+
 
 // Modificar un post
 export const updatePost = async (req, res) => {
   try {
-    const {id, name_device, resolution_eye, relase_date, field_view, weight, relase_price } = req.body;
+    const { name_device, resolution_eye, relase_date, field_view, weight, relase_price } = req.body;
     await pool.query(
-      'UPDATE blogs SET id = ?, name_device = ?, resolution_eye = ?, relase_date = ?, field_view = ?, weight = ?, relase_price = ? WHERE id = ?',
-      [name_device, resolution_eye, relase_date, field_view, weight, relase_price, req.params.id]
+      'UPDATE blogs SET name_device = ?, resolution_eye = ?, relase_date = ?, field_view = ?, weight = ?, relase_price = ? WHERE id = ?',
+      [name_device, resolution_eye, relase_date, field_view, weight, relase_price, req.params.postId]
     );
-    res.status(200).json({ message: 'Post actualizado' });
+    res.status(200).json({ 
+      code: 200,
+      message: 'Post actualizado exitosamente'
+    });
   } catch (err) {
     console.error('Error al actualizar el post:', err);
-    res.status(500).json({ message: 'Error al actualizar el post', error: err.message });
+    res.status(500).json({ 
+      code: 500,
+      message: 'Error al actualizar el post',
+      description: err.message 
+    });
   }
 };
+
+
 
 // Eliminar un post
 export const deletePost = async (req, res) => {
   try {
-    await pool.query('DELETE FROM blogs WHERE id = ?', [req.params.id]);
-    res.status(200).json({ message: 'Post eliminado' });
+    await pool.query('DELETE FROM blogs WHERE id = ?', [req.params.postId]);
+    res.status(200).json({ 
+      code: 200,
+      message: 'Post eliminado exitosamente'
+    });
   } catch (err) {
-    res.status(500).json({ message: 'Error al eliminar el post', error: err });
+    res.status(500).json({ 
+      code: 500,
+      message: 'Error al eliminar el post',
+      description: err.message 
+    });
   }
 };
