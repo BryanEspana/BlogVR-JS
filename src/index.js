b41af072e1082a9c1c424dabac19381d8384d02c
Bryan España <bryanespana21550@gmail.com>
2024-03-10 13:45:01 -0600

    Implementación de swagger

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
 import postRoutes from './routes/postRoutes.js';
 
-const app = express();
-const port = 3000;
 
+
+const app = express();
 app.use(express.json());
 app.use('/posts', postRoutes);
 
+//Implementar swagger
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Documentación de la API de BlogVR',
+      version: '1.0.0',
+      description: 'Esta es la documentación de la API para BlogVR.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor de Desarrollo',
+      },
+    ],
+  },
+  apis: ['./src/routes/postRoutes.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 app.use((req, res, next) => {
   const error = new Error(`El método ${req.method} no está implementado para ${req.originalUrl}`);
-  error.status = 501; // Asigna el código de estado 501 al error
-  next(error); // Pasa el error al manejador de errores
+  error.status = 501; 
+  next(error); 
 });
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500).json({
     message: error.message,
-    error: error.status ? undefined : error, // Omite la pila de errores en producción para errores no esperados
+    error: error.status ? undefined : error,
   });
 });
-// Después de todas las definiciones de rutas
+
 app.use((req, res, next) => {
   res.status(404).json({
     message: "El endpoint solicitado no existe."
@@ -29,7 +55,13 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+
 
 export default app;
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -16,3 +16,146 @@ router.put('/:postId', updatePost);
 router.delete('/:postId', deletePost);
 
 export default router;
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - name_device
+ *         - resolution_eye
+ *         - relase_date
+ *         - field_view
+ *         - weight
+ *         - relase_price
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: El ID auto generado del post.
+ *         name_device:
+ *           type: string
+ *           description: Nombre del dispositivo.
+ *         resolution_eye:
+ *           type: string
+ *           description: Resolución por ojo del dispositivo VR.
+ *         relase_date:
+ *           type: string
+ *           format: date
+ *           description: Fecha de lanzamiento del dispositivo.
+ *         field_view:
+ *           type: integer
+ *           description: Campo de visión del dispositivo.
+ *         weight:
+ *           type: number
+ *           description: Peso del dispositivo.
+ *         relase_price:
+ *           type: number
+ *           description: Precio de lanzamiento del dispositivo.
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Lista todos los posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: El listado de todos los posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Obtiene un post por su ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: El ID del post
+ *     responses:
+ *       200:
+ *         description: Un post encontrado por su ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: Post no encontrado
+ */
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Crea un nuevo post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       201:
+ *         description: Post creado exitosamente
+ */
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Actualiza un post por su ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: El ID del post a actualizar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: Post actualizado exitosamente
+ *       404:
+ *         description: Post no encontrado
+ */
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     summary: Elimina un post por su ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: El ID del post a eliminar
+ *     responses:
+ *       200:
+ *         description: Post eliminado exitosamente
+ *       404:
+ *         description: Post no encontrado
+ */
